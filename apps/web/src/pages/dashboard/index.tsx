c5d0028f2d4d0c753d444212f9e43d2d8cbec471
Jame <jamesrading26@gmail.com>
2026-07-08 18:32:04 +1000

    modify: optimize the layout of whole dashboard page

--- apps/web/src/pages/dashboard/index.tsx
+++ apps/web/src/pages/dashboard/index.tsx
@@ -15,12 +15,12 @@ export default function DashboardPage() {
         <div className="flex flex-col gap-4 lg:col-span-2">
           <ScoreCards />
           <ReturnsCards />
+          <ComparisonChart />
         </div>
-        <LatestDecisionCard />
-      </div>
-      <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-        <ComparisonChart />
-        <RiskCard />
+        <div className="flex flex-col gap-4">
+          <LatestDecisionCard />
+          <RiskCard />
+        </div>
       </div>
     </div>
   )
